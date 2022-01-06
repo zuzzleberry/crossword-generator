82b7e75dcaba6a7c1302160ad61ac21b91fd9d55
Adrian Zuanic <azuanic@gmail.com>
2022-01-06 14:11:48 +1100

    commit 0 - basic project setup of functions to take a list of words and generate a matrix twice the size of the longest word

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [wordList, setWordList] = React.useState([
+    {
+      word: "brazen",
+      definition: "made of brass",
+    },
+    {
+      word: "extant",
+      definition: "still in existence",
+    },
+    {
+      word: "harbinger",
+      definition: "smth or smb that foretells the coming of",
+    },
+    {
+      word: "coddle",
+      definition: "treat with care and tenderness",
+    },
+    {
+      word: "serration",
+      definition: "having a toothed edge",
+    },
+    {
+      word: "molt",
+      definition: "molt",
+    },
+  ]);
+  
+  React.useEffect(() => {
+    crosswordGenerator(wordList)
+  })
+
+  return <div className="App">HI</div>;
 }
 
 export default App;
+
+
+
+const crosswordGenerator = (wordList) => {
+  console.log(wordList);
+
+
+  const size = findLength(wordList);
+  console.log(size);
+
+
+  const matrix = generateMatrix(size * 2);
+  console.log(matrix);
+
+  const crosswordArrangement = findArrangement(wordList, matrix);
+}
+
+const findLength = (wordList) => {
+  let highest = 0;
+  wordList.forEach((wordObj) => {
+    if (wordObj.word.length > highest) {
+      highest = wordObj.word.length;
+    }
+  })
+  return highest;
+}
+
+const generateMatrix = (size) => {
+  const matrix = new Array(size);
+  for (var i = 0; i < matrix.length; i++) {
+    matrix[i] = new Array(size);
+  }
+  return matrix
+}
+
+const findArrangement = (wordList, matrix) => {
+
+}
